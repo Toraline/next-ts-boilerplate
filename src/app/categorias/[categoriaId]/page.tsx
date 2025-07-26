4ef75cf415152e3ec40842a6c500ec229b6b6df6
Juliana Negreiros <ju_bsnegreiros@hotmail.com>
2025-07-26 19:15:24 +0000

    fix: page example use loading

--- src/app/categorias/[categoriaId]/page.tsx
+++ src/app/categorias/[categoriaId]/page.tsx
@@ -1,5 +1,3 @@
-import { use } from "react";
-
 const getData = async () => {
   await new Promise((resolve) => {
     setTimeout(resolve, 3000);
@@ -7,14 +5,14 @@ const getData = async () => {
   return true;
 };
 
-export default function Page({
+export default async function Page({
   params,
 }: {
   params: { slug: string; categoriaId: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  const { categoriaId, slug } = params;
-  use(getData());
+  const { categoriaId, slug } = await params;
+  await getData();
 
   return (
     <h1>
