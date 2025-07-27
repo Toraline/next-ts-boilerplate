ef3087253f274b86aecfd8b84c431190e0840373
Juliana Negreiros <ju_bsnegreiros@hotmail.com>
2025-07-27 00:51:00 +0000

    feat: add Clerk authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,39 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignOutButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>Layout level{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <header>
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton afterSwitchSessionUrl="/" showName />
+              <SignOutButton redirectUrl="https://studious-system-97xvrjjwwq52pwq9-3000.app.github.dev/" />
+            </SignedIn>
+          </header>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
