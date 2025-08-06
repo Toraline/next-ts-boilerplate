3034b567d300519020c36b2412650b9e609a9679
Lana <lanalumi97@gmail.com>
2025-08-06 17:45:43 -0300

    custom component color

---
+++ src/app/material-ui/components/Button.tsx
@@ -0,0 +1,8 @@
+"use client";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
+export const CustomButton = styled(Button)({
+  color: "red",
+  backgroundColor: "green",
+}) as typeof Button;
