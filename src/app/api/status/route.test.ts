01b58bd3ffa3258bacd497dfca220ffbb50b5eb0
Juliana Negreiros <ju_bsnegreiros@hotmail.com>
2025-08-11 18:42:01 +0000

    feat: add GET /api/status endpoint

---
+++ src/app/api/status/route.test.ts
@@ -0,0 +1,19 @@
+describe("GET /api/status", () => {
+  test("should return status ok", async () => {
+    const response = await fetch(process.env.API_URL + "/api/status");
+    expect(response.ok).toBe(true);
+    expect(response.status).toBe(200);
+
+    const data = await response.json();
+    const expectedStatus = {
+      dependencies: {
+        database: {
+          server_version: "16.9 (Debian 16.9-1.pgdg120+1)",
+          connections: 1,
+          max_connections: 100,
+        },
+      },
+    };
+    expect(data).toEqual(expectedStatus);
+  });
+});
