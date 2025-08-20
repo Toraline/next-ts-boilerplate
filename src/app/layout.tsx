b4d7d5dbc25dfb6db4d82f00d4019c002e3e79f0
Juliana Negreiros <ju_bsnegreiros@hotmail.com>
2025-08-20 02:52:32 +0000

    build: remove clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,18 @@
 import { ReactNode } from "react";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignOutButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+
 import "../styles/globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>
-          <header>
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton afterSwitchSessionUrl="/" showName />
-              <SignOutButton />
-            </SignedIn>
-          </header>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body>
+        <header>
+          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </header>
+        {children}
+      </body>
+    </html>
   );
 }
