cfdd40c68b89592102bcade9b79ad21241d354da
Lana <lanalumi97@gmail.com>
2025-08-22 14:06:54 -0300

    creating openapi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <h1>My public Page</h1>;
+import generateOpenApiSpec from "@omer-x/next-openapi-json-generator";
+import { Category } from "../models/Category";
+import { ApiReferenceReact } from "@scalar/api-reference-react";
+
+export default async function Page() {
+  const spec = await generateOpenApiSpec({
+    Category,
+  });
+  return (
+    <body>
+      <h1>My public Page</h1>
+      <ApiReferenceReact spec={spec}></ApiReferenceReact>;
+    </body>
+  );
 }
