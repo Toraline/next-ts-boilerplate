4dccebfffa665bc3460ff7c442bc543032a27f3b
Lana <lanalumi97@gmail.com>
2025-08-27 17:02:16 -0300

    uninstall tailwindcss, postcss and autoprefixer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
-import "../styles/globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -19,9 +18,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <SignedOut>
               <SignInButton />
               <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
+                <button>Sign Up</button>
               </SignUpButton>
             </SignedOut>
             <SignedIn>
