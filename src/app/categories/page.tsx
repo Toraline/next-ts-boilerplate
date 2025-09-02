b602fa52355a2465ebfb1d2893b8e037a90ceedc
Lana <lanalumi97@gmail.com>
2025-09-02 19:05:32 -0300

    add button Link

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>Categories</h1>;
+  return (
+    <>
+      <h1>Categories</h1>
+      <Link href="/categories/new">Create Category</Link>
+      {/* <Link href="/categories/categoryId">Edit Category</Link> */}
+    </>
+  );
 }
