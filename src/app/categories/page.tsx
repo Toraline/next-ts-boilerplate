9b6501f8460768edbe27dd512e16f9171ec79274
Lana <lanalumi97@gmail.com>
2025-09-03 14:18:11 -0300

    linking  and

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import Link from "next/link";
+import CategoriesTable from "./components/CategoriesTable/CategoriesTable";
 
 export default function Page() {
+  const categories = [{ name: "Categoria 1", slug: "categoria1" }];
+  const isLoading = false;
   return (
     <>
       <h1>Categories</h1>
+      <CategoriesTable categories={categories} loading={isLoading} />
       <Link href="/categories/new">Create Category</Link>
       {/* <Link href="/categories/categoryId">Edit Category</Link> */}
     </>
