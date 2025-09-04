ec16b581742ce29cc99857f7c0e4193dfde81781
Lana <lanalumi97@gmail.com>
2025-09-04 13:31:14 -0300

    TOR-50-Add Delete button

--- src/app/categories/components/CategoriesTable/CategoriesTable.tsx
+++ src/app/categories/components/CategoriesTable/CategoriesTable.tsx
@@ -1,16 +1,27 @@
+"use client";
+
 import { Category } from "modules/categories";
+import { deleteCategory } from "modules/categories/categories.api";
 import Link from "next/link";
+import { useState } from "react";
 
 type CategoriesTableProps = {
-  categories: Category[];
+  initialState: Category[];
   loading: boolean;
 };
 
-export default function CategoriesTable({ categories, loading }: CategoriesTableProps) {
+export default function CategoriesTable({ initialState, loading }: CategoriesTableProps) {
+  const [categories, setCategories] = useState(initialState);
+  const onDelete = async (slug) => {
+    await deleteCategory(slug);
+    const filteredCategories = categories.filter((categories) => categories.slug !== slug);
+    setCategories(filteredCategories);
+    console.log(filteredCategories);
+  };
   return (
     <ul>
       {loading && "is loading..."}
-      {categories.length && "No categories found"}
+      {categories.length == 0 && "No categories found"}
       {!loading &&
         categories.length > 0 &&
         categories.map((category) => (
@@ -19,6 +30,9 @@ export default function CategoriesTable({ categories, loading }: CategoriesTable
             <p> {category.slug}</p>
             <p>{category.description}</p>
             <Link href={`/categories/${category.slug}`}>Edit Category</Link>
+            <button id="delete-button" type="button" onClick={() => onDelete(category.slug)}>
+              Delete
+            </button>
           </li>
         ))}
     </ul>
