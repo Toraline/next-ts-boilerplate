3ca01b25558bcd88f9d3876752a1638a9f5f9f90
Juliana Negreiros <negreirosjuu@gmail.com>
2025-09-09 19:19:06 -0300

    add link to categories

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import { ReactNode } from "react";
-
-import "../styles/globals.css";
+import Link from "next/link";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
         <header>
-          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-            Sign Up
-          </button>
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/categories">Categories</Link>
+          </nav>
         </header>
         {children}
       </body>
