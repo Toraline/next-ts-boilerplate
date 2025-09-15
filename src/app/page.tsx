52b3d1b4b17e52d83c78f98345e4b1ce5ac41b03
Lana <lanalumi97@gmail.com>
2025-09-15 19:29:46 -0300

    position fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import "./styles/page.style.css";
 import { useState } from "react";
 import { Item } from "../ui/Item";
+
 export default function Page() {
   const [content, setContent] = useState("initial value");
   const [isDone, setIsDone] = useState(true);
   return (
-    <>
+    <div className="page__container">
       <h1>My public Page</h1>
       <Item
         content={content}
@@ -16,6 +18,6 @@ export default function Page() {
         onDelete={() => console.log("deletou")}
         onEdit={() => console.log("editou")}
       />
-    </>
+    </div>
   );
 }
