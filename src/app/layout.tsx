556df81f96f183f46bf037c986ce12a487251166
Lana <lanalumi97@gmail.com>
2025-09-15 15:44:11 -0300

    Design menu lateral

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import { ReactNode } from "react";
 import Link from "next/link";
 import { Sidebar } from "components/Sidebar/Sidebar";
+import "./styles/layout.style.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
         <header>
-          <nav>
+          <nav className="header__nav">
             <Sidebar />
             <Link href="/">Home</Link>
             <Link href="/categories">Categories</Link>
