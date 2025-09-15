93dcf50b0bbd55b1c002ad013d9e7a8bd8fa71b0
Lana <lanalumi97@gmail.com>
2025-09-15 19:19:49 -0300

    svg icon edit

--- src/ui/Icons/PlusSign.tsx
+++ src/ui/Icons/PlusSign.tsx
@@ -4,7 +4,7 @@ export const PlusSign = (props) => {
       xmlns="http://www.w3.org/2000/svg"
       width="10"
       height="10"
-      viewBox="0 0 10 10"
+      viewBox="1 1 8 8"
       fill="none"
       {...props}
     >
