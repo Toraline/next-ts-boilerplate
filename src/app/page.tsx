6e9c57d361f146d33bca4b34da970cf2ab79c38b
Lana <lanalumi97@gmail.com>
2025-09-16 18:57:35 -0300

    textarea design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import { Item } from "../ui/Item";
+import { TextArea } from "ui/TextArea/TextArea";
+
 export default function Page() {
   const [content, setContent] = useState("initial value");
   const [isDone, setIsDone] = useState(true);
@@ -16,6 +18,8 @@ export default function Page() {
         onDelete={() => console.log("deletou")}
         onEdit={() => console.log("editou")}
       />
+      <h1>Componente TextArea</h1>
+      <TextArea label="Description" id="textarea" placeholder="Daily description" />
     </>
   );
 }
