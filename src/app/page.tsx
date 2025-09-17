acf8b20d62984bd59e0d0b2e904fa4596eaabb24
Lana <lanalumi97@gmail.com>
2025-09-17 15:26:57 -0300

    field design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { Item } from "../ui/Item";
+import { Field } from "ui/Field";
 export default function Page() {
   const [content, setContent] = useState("initial value");
   const [isDone, setIsDone] = useState(true);
@@ -16,6 +17,11 @@ export default function Page() {
         onDelete={() => console.log("deletou")}
         onEdit={() => console.log("editou")}
       />
+      <h1>Componente Field</h1>
+      <h2> Com label</h2>
+      <Field label="Category name" placeholder="Type your category name" />
+      <h2>Sem label</h2>
+      <Field placeholder="Type your task name here" variant="nolabel" />
     </>
   );
 }
