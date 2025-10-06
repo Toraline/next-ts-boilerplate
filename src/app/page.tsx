5e2e7f89d27e20d3b949d1cac2a062f37d4801de
Lanalumi <lanalumi97@gmail.com>
2025-10-06 18:39:55 -0300

    TOR-67-Design field (#31)
    * field design
    
    * fix Item bug
    
    * add css updates and icon position fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { TextArea } from "ui/TextArea/TextArea";
 import "./styles/page.style.css";
 import { useState } from "react";
+import { Field } from "ui/Field";
 import { Item } from "ui/Item";
 import { Button } from "ui/Button/Button";
 import "styles/global.css";
@@ -23,12 +24,17 @@ export default function Page() {
       />
 
       <h1>Componente Botão</h1>
-      <div className="button-container">
+      <div className="button__container">
         <Button>Padrão</Button>
         <Button size="sm">Pequeno</Button>
         <Button variant="transparent">Transparente</Button>
       </div>
 
+      <h1>Componente Field</h1>
+      <h2> Com label</h2>
+      <Field label="Category name" placeholder="Type your category name" />
+      <h2>Sem label</h2>
+      <Field placeholder="Type your task name here" />
       <h1>Componente TextArea</h1>
       <h2>Com label</h2>
       <TextArea label="Description" id="textarea" placeholder="Daily description" />
