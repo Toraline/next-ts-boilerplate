96b80c44bb15770d6dc1686edfef220e193f600f
Juliana Negreiros <ju_bsnegreiros@hotmail.com>
2025-10-14 15:30:10 -0300

    bo-46 update button component

--- src/global/ui/Button/Button.stories.ts
+++ src/global/ui/Button/Button.stories.ts
@@ -7,6 +7,7 @@ import { Button } from "./Button";
 const meta = {
   title: "UI/Button",
   component: Button,
+  tags: ["autodocs"],
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -16,5 +17,29 @@ export const Primary: Story = {
   args: {
     children: "Button",
     onClick: fn(),
+  },
+};
+
+export const Small: Story = {
+  args: {
+    children: "Button",
+    onClick: fn(),
+    size: "sm",
+  },
+};
+
+export const Transparent: Story = {
+  args: {
+    children: "Button",
+    onClick: fn(),
+    variant: "transparent",
+  },
+};
+
+export const Link: Story = {
+  args: {
+    children: "Button",
+    onClick: fn(),
+    href: "/",
   },
 };
