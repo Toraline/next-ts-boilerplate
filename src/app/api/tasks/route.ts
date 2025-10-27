60f0cb6851bea36b507d36b0be57ed2691d7d3c5
Lanalumi <lanalumi97@gmail.com>
2025-10-27 13:04:49 -0300

    BO-112-API: List tasks (#53)
    * add create task
    
    * bo-113-API:get task
    
    * bo-112- added an api to list the tasks

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getErrorMessage, getHttpStatus } from "lib/http/errors";
-import { createTask } from "modules/tasks/server/service";
+import { createTask, listTasks } from "modules/tasks/server/service";
 
 export const runtime = "nodejs";
 
@@ -10,6 +10,19 @@ export async function POST(request: Request) {
 
     const createdTask = await createTask(task);
     return NextResponse.json(createdTask, { status: 201 });
+  } catch (e) {
+    return NextResponse.json({ error: getErrorMessage(e) }, { status: getHttpStatus(e) });
+  }
+}
+
+export async function GET(req: Request) {
+  try {
+    const url = new URL(req.url);
+    const params = Object.fromEntries(url.searchParams.entries());
+
+    const data = await listTasks(params);
+
+    return NextResponse.json(data);
   } catch (e) {
     return NextResponse.json({ error: getErrorMessage(e) }, { status: getHttpStatus(e) });
   }
