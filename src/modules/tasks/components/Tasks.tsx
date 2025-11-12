e6a0fc04e1b78fc51e3c6a4b1ebc8526a194ed5f
Lana <lanalumi97@gmail.com>
2025-11-12 18:46:31 -0300

    bo-318-update tasks tailwind style

--- src/modules/tasks/components/Tasks.tsx
+++ src/modules/tasks/components/Tasks.tsx
@@ -6,30 +6,37 @@ import { GLOBAL_UI } from "global/constants";
 import { useTasksList } from "../hooks/useTasksList";
 import { FormNewTask } from "./FormNewTask/FormNewTask";
 import FormEditTask from "./FormEditTask/FormEditTask";
+import { useState } from "react";
 
 type TasksProps = {
   categoryId: string;
 };
 
 export const Tasks = ({ categoryId }: TasksProps) => {
+  const [isCreating, setIsCreating] = useState(false);
   const { data, isLoading, error } = useTasksList({ categoryId });
 
+  const toggleCreateMode = () => {
+    setIsCreating(!isCreating);
+  };
   return (
-    <div>
+    <div className="flex-col flex gap-2">
       <div className=" flex align-middle flex-row justify-between">
         <h2 className="text-2xl font-semibold">{CATEGORIES_UI.HEADERS.TASKS}</h2>
-        <Button size="sm">{GLOBAL_UI.BUTTONS.NEW_TASK}</Button>
+        <Button onClick={toggleCreateMode} size="sm">
+          {GLOBAL_UI.BUTTONS.NEW_TASK}
+        </Button>
       </div>
       {isLoading && <p>Loading...</p>}
       {error && <p>{error.message}</p>}
       {data?.items && (
-        <div>
+        <div className="flex-col flex gap-2">
           {data?.items.map((task) => (
-            <div className=" flex gap-2">
+            <div>
               <FormEditTask taskId={task.id} initialState={task} />
             </div>
           ))}
-          <FormNewTask categoryId={categoryId} />
+          {isCreating ? <FormNewTask categoryId={categoryId} /> : ""}
         </div>
       )}
     </div>
