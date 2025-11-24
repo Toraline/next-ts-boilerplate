22df84bc1837278489fec7ad381e4cdc2e13200a
Lana <lanalumi97@gmail.com>
2025-11-24 14:33:49 -0300

    bo-365-comment fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { FormNewPermission } from "modules/permissions/components/FormNewPermission/FormNewPermission";
-
 export default function Page() {
   return (
     <div>
       <h1>My public Page</h1>
-      <FormNewPermission />
     </div>
   );
 }
