836b6495d7074a1b8d48d431230351b2d954048e
Lanalumi <lanalumi97@gmail.com>
2025-12-02 09:45:04 -0300

    bo-368- permission list (#89)

--- src/modules/permissions/server/types.ts
+++ src/modules/permissions/server/types.ts
@@ -1,6 +1,18 @@
 import { z } from "zod";
-import { createPermissionSchema, permissionPublicSchema } from "../schema";
+import {
+  createPermissionSchema,
+  listPermissionsQuerySchema,
+  listPermissionsResponseSchema,
+  permissionPublicSchema,
+  permissionsListFiltersSchema,
+} from "../schema";
 
 export type createPermission = z.infer<typeof createPermissionSchema>;
 
 export type Permission = z.infer<typeof permissionPublicSchema>;
+
+export type ListPermissionsResponse = z.infer<typeof listPermissionsResponseSchema>;
+
+export type ListPermissionsQuery = z.infer<typeof listPermissionsQuerySchema>;
+
+export type PermissionsListFilters = z.infer<typeof permissionsListFiltersSchema>;
