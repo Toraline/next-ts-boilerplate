dccc6b8e0144cc20c9416fcfdd7b6663db45da04
Lana <lanalumi97@gmail.com>
2025-12-10 18:16:44 -0300

    added the delete button to editStateRole

--- src/modules/roles/components/EditStateRole/EditStateRole.tsx
+++ src/modules/roles/components/EditStateRole/EditStateRole.tsx
@@ -4,9 +4,32 @@ import { ROLES_UI } from "modules/roles/constants/ui";
 import { useRole } from "modules/roles/hooks/useRole";
 import FormEditRole from "../FormEditRole.tsx/FormEditRole";
 import { ROLE_ERRORS } from "modules/roles/constants/errors";
+import { useDeleteRole } from "modules/roles/hooks/useDeleteRole";
+import { toast } from "sonner";
+import { ROLE_SUCCESSES } from "modules/roles/constants";
+import { GLOBAL_UI } from "global/constants";
+import { Button } from "global/ui";
+import { useRouter } from "next/navigation";
 
 export default function EditStateRole({ roleId }: { roleId: string }) {
   const { data: role, isLoading, error } = useRole(roleId);
+
+  const deleteRoleMutation = useDeleteRole();
+  const router = useRouter();
+
+  const onDelete = async () => {
+    if (!role || !confirm(ROLES_UI.CONFIRMATIONS.DELETE_ROLE)) return;
+
+    deleteRoleMutation.mutate(role.id, {
+      onSuccess: () => {
+        toast.success(ROLE_SUCCESSES.DELETE_ROLE_SUCCESS);
+        router.push("/admin/roles");
+      },
+      onError: () => {
+        toast.error(ROLE_ERRORS.DELETE_ROLE_ERROR);
+      },
+    });
+  };
 
   if (isLoading) {
     return <div>{ROLES_UI.LOADING.LOADING_ROLE}</div>;
@@ -30,6 +53,15 @@ export default function EditStateRole({ roleId }: { roleId: string }) {
         <h1 className="text-3xl font-semibold">{role.name}</h1>
       </div>
       <FormEditRole initialState={role} roleId={roleId} />
+      <Button
+        id="delete-button"
+        type="button"
+        onClick={onDelete}
+        disabled={deleteRoleMutation.isPending}
+      >
+        Delete
+        {deleteRoleMutation.isPending && GLOBAL_UI.BUTTONS.DELETING}
+      </Button>
     </div>
   );
 }
